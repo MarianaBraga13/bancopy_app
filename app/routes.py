442397e9237b4f05e0c2b2aa3921b9ba11cb162a
MarianaBraga13 <maribragad@gmail.com>
2025-07-27 12:02:48 -0300

    Aprendendo sobre Postman

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,14 @@ main = Blueprint('main', __name__)
 # importante lembrar que, numa arquitetura REST quanto a query é feita
 # a resposta para o user é salva temporariamente no jsonify
 
+@main.route('/usuario', methods=['POST'])
+def criar_usuario():
+    dados = request.get.jason() # Pega o json da requisição
+    novo_usuario = Usuario(nome=dados['nome'])
+    db.session.add(novo_usuario) # Prepara para salvar
+    db.session.commit() # Salva no banco
+    return jsonify({'mensagem' : "Usuário (a) criado (a) com sucesso!"}), 201 # Resposta do JSON
+
 @main.route('/usuario/<int:user_id>', methods=['GET'])
 def get_usuario(user_id):   
     user = Usuario.query.get(user_id)
@@ -22,5 +30,5 @@ def get_usuario(user_id):
     
     return jsonify({"error": "Usuário (a) não encontrado (a)"}), 404
     
-# refazer tudo aqui de routes amanhã    
+  
 
